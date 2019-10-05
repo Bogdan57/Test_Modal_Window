fdd95072bb051ab9df0dad5d0680ca21da25fe78
Bogdan Novikov <fikis57@mail.ru>
2019-10-05 20:21:12 +0300

    new commit

--- task.js
+++ task.js
@@ -1,6 +1,5 @@
 let modal_main = document.querySelector('#modal_main');
 let modal__close = document.querySelectorAll('div.modal__close');
-let show_success = document.querySelector('.show-success');
 let modal_success = document.querySelector('#modal_success');
 modal_main.className = 'modal modal_active';
 
@@ -11,7 +10,31 @@ show_success.onclick = function(){
     modal_main.className = 'modal'
     modal_success.className = 'modal modal_active';
 }
-modal__close[1].onclick = function(){
-    modal_success.className = 'modal';
-    modal_main.className = 'modal';
-}
+
+show_success.onclick = function(){
+	function showModal(id){
+		$(document.body).addClass('is-open-modal');
+		$(id).addClass('is-open');
+		console.log(id);
+	}
+	function hideModals(){
+		$(document.body).removeClass('is-open-modal');
+		$('.modal').removeClass('is-open');
+	}
+
+	$(".modal-open").on('click', function(e){
+		console.log(e);
+		showModal('#modal');
+ });
+	
+	$(document).on('click', function(e){
+		if (!(
+		($(e.target).parents('.modal__content').length)
+		||	($(e.target).hasClass('modal__content'))
+		||	($(e.target).hasClass('modal-open')))
+		) {
+			hideModals();
+		}
+	});
+	
+};
